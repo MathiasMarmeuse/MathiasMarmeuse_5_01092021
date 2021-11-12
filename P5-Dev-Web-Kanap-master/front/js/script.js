8f945f80ea4a6cc8873fe2743e2f150d1317c815
Marmeuse <Mathias>
2021-11-12 09:20:25 +0100

    first commit, affichage des produits page d'accueil

---
+++ P5-Dev-Web-Kanap-master/front/js/script.js
@@ -0,0 +1,33 @@
+function displayCard(array) {
+  let section = document.getElementById("items");
+
+  for (let object of array) {
+    let title = "<h3 class='productName'>" + object.name + "</h3>";
+    let description =
+      "<p class='productDescription'>" + object.description + "</p>";
+    let image =
+      "<img src='" + object.imageUrl + "' alt='" + object.altTxt + "'>";
+
+    section.innerHTML +=
+      "<a href='./product.html?id=" +
+      object._id +
+      "'><article> " +
+      image +
+      title +
+      description +
+      "</article></a>";
+  }
+}
+
+fetch("http://localhost:3000/api/products")
+  .then(function (res) {
+    if (res.ok) {
+      return res.json();
+    }
+  })
+  .then(function (array) {
+    displayCard(array);
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
